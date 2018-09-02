b923d668e08f6cf16363ed72d6524efbc5434f15
James Still <40690517+james-still@users.noreply.github.com>
2018-09-02 14:40:34 +0100

    Large code minification

--- api.php
+++ api.php
@@ -1,41 +1,28 @@
 <?php
-if(isset($_GET['name'])) {
+// language to wiki article name
+$languages = array(
+  "HTML" => "HTML",
+  "PHP" => "PHP",
+  "Cascading_Style_Sheets" => "CSS",
+  "SQL" => "SQL",
+  "JavaScript" => "JavaScript",
+  "JSON" => "JSON",
+  "Python_(programming_language)" =>"Python"
+);
 
-  function jsonInfo($description) {
-    echo json_encode(array(
-      "Language" => $_GET['name'],
-      "Description" => $description
-    ));
-  }
+if(isset($_GET['name']) && in_array($_GET['name'], $languages)) {
+  // get wiki description of that language
+  $description = json_decode(file_get_contents("https://en.wikipedia.org/w/api.php?action=opensearch&search=".array_search($_GET['name'], $languages)."&limit=1&prop=pageimages&format=json"));
+  $description = $description[2][0];
 
-  switch($_GET['name']) {
-    case "HTML":
-      jsonInfo("Hypertext Markup Language, a standardized system for tagging text files to achieve font, colour, graphic, and hyperlink effects on World Wide Web pages.");
-      break;
-    case "PHP":
-      jsonInfo("PHP: Hypertext Preprocessor is a server-side scripting language designed for Web development, but also used as a general-purpose programming language.");
-      break;
-    case "CSS":
-      jsonInfo("Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.");
-      break;
-    case "SQL":
-      jsonInfo("SQL is a domain-specific language used in programming and designed for managing data held in a relational database management system, or for stream processing in a relational data stream management system.");
-      break;
-    case "JavaScript":
-      jsonInfo("JavaScript, often abbreviated as JS, is a high-level, interpreted programming language. It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm.");
-      break;
-    case "JSON":
-      jsonInfo("In computing, JavaScript Object Notation or JSON is an open-standard file format that uses human-readable text to transmit data objects consisting of attribute–value pairs and array data types.");
-      break;
-    case "Python":
-      jsonInfo("Python is an interpreted high-level programming language for general-purpose programming. Created by Guido van Rossum and first released in 1991, Python has a design philosophy that emphasizes code readability, notably using significant whitespace.");
-      break;
-    default:
-      echo "NO VALID LANGAUGE PROVIDED";
-      break;
-  }
+  // return json content
+  echo json_encode(array(
+    "Language" => $_GET['name'],
+    "Description" => $description,
+    "Logo" => "https://james-still.com/images/".$_GET['name'].".png"
+  ), JSON_PRETTY_PRINT);
 } else {
-  echo "NO VALID LANGAUGE PROVIDED";
+  // error message, no valid language was provided
+  echo "NO VALID LANGUAGE PROVIDED";
 }
 ?>
-
