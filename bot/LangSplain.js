18e13fbf0c08e06a9908c2faaca0438d44dd824a
James Still <40690517+james-still@users.noreply.github.com>
2018-09-02 14:41:20 +0100

    Large code minification

--- bot/LangSplain.js
+++ bot/LangSplain.js
@@ -4,25 +4,37 @@ const client = new Discord.Client();
 
 function createEmbed(msg, language) {
   request('https://james-still.com/api.php?name=' + language, function(error, response, body) {
-    if (!error && response.statusCode == 200 && body != "NO VALID LANGAUGE PROVIDED") {
+    if (!error && response.statusCode == 200 && body != "NO VALID LANGUAGE PROVIDED") {
       var jsonInfo = JSON.parse(body);
       msg.channel.send({
         embed: {
           color: 0xff000e,
           title: language,
-          description: jsonInfo.Description
+          description: jsonInfo.Description,
+          "thumbnail": {
+            "url": jsonInfo.Logo
+          }
         }
       });
+    } else {
+      helpMessage(msg);
     }
   });
 }
 
 function helpMessage(msg) {
   msg.channel.send({
-    embed: {
-      color: 0xff000e,
-      title: "Invalid Command",
-      description: "<>HTML | <>PHP | <>CSS | <>SQL | <>JavaScript | <>JSON | <>Python"
+    "embed": {
+      "title": "ABOUT LANGSPLAIN",
+      "description": "LangSplain is a bot that provides you valuable information about languages, type in a command below using the prefix `<>` to learn about a specific language.",
+      "color": 0xff000e,
+      "fields": [{
+        "name": "COMMANDS",
+        "value": "`<>HTML`\n\n`<>PHP`\n\n`<>CSS`\n\n`<>SQL`\n\n`<>JavaScript`\n\n`<>JSON`\n\n`<>Python`"
+      }],
+      "footer": {
+        "text": "more languages will be added soon..."
+      }
     }
   });
 }
@@ -32,34 +44,9 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.author.bot) return;
   if (msg.content.startsWith("<>")) {
-    switch (msg.content) {
-      case "<>HTML":
-        createEmbed(msg, "HTML");
-        break;
-      case "<>PHP":
-        createEmbed(msg, "PHP");
-        break;
-      case "<>CSS":
-        createEmbed(msg, "CSS");
-        break;
-      case "<>SQL":
-        createEmbed(msg, "SQL");
-        break;
-      case "<>JavaScript":
-        createEmbed(msg, "JavaScript");
-        break;
-      case "<>JSON":
-        createEmbed(msg, "JSON");
-        break;
-      case "<>Python":
-        createEmbed(msg, "Python");
-        break;
-      default:
-        helpMessage(msg);
-        break;
-    }
+    if (msg.author.bot) return;
+    createEmbed(msg, msg.content.replace("<>", ""));
   }
 });
 
