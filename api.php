1afb2f56cc09d06f974a7b91d465539e4ca04589
James Still <40690517+james-still@users.noreply.github.com>
2018-09-02 15:42:18 +0100

    Added language support

--- api.php
+++ api.php
@@ -7,7 +7,11 @@ $languages = array(
   "SQL" => "SQL",
   "JavaScript" => "JavaScript",
   "JSON" => "JSON",
-  "Python_(programming_language)" => "Python"
+  "Python_(programming_language)" => "Python",
+  "Lua_(programming_language)" => "Lua",
+  "Java_(programming_language)" => "Java",
+  "C%2B%2B" => "CPP",
+  "C_Sharp_(programming_language)" => "CSharp"
 );
 
 if(isset($_GET['name']) && in_array($_GET['name'], $languages)) {
